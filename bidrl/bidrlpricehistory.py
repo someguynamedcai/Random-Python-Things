babec917293fc702582570db0a4ea66bdfaba41a
Eric Cai <someguynamedcai@gmail.com>
2022-06-16 11:27:00 -0700

    Implemented opening camelcamelcamel page on search
    New feature that I added will not be useful in 90% of cases as item titles are not specific enough to warranted for an amazon search. Only cases I can think of are games and lego items.

--- bidrl/bidrlpricehistory.py
+++ bidrl/bidrlpricehistory.py
@@ -2,9 +2,8 @@
 
 import json
 import requests
-import datetime
-import time
 import webbrowser
+import re
 
 #Spage = requests.get("https://www.bidrl.com/api/landingPage/sacramento-2")
 Spage = requests.get("https://www.bidrl.com/api/landingPage/cesar-lua-2")
@@ -22,6 +21,9 @@ Gjson = Gpage.json()
 
 #Json page which has all auction item information
 getitems = "https://www.bidrl.com/api/getitems"
+cccprice = "https://camelcamelcamel.com/search?sq="
+edge_path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
 
 #Finding out the auction id of most recent gallery" that is not closed
 for Sauctions in Sjson['auctions'].keys():
@@ -79,6 +81,9 @@ CItem1 = CPost['items'][0]['title']
 NItem1 = NPost['items'][0]['title']
 GItem1 = GPost['items'][0]['title']
 
+
+Cleanquery = re.compile(re.escape('see pictures'), re.IGNORECASE)
+
 def Itemcheck(location):
     while (True):
         try:
@@ -91,25 +96,43 @@ def Itemcheck(location):
                     case 'S':
                         print ("Item number " + number + " is titled " + SPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + SPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", SPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
                     case 'E':
                         print ("Item number " + number + " is titled " + EPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + EPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", EPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
                     case 'R':
                         print ("Item number " + number + " is titled " + RPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + RPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", RPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
                     case 'C':
                         print ("Item number " + number + " is titled " + CPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + CPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", CPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
                     case 'N':
                         print ("Item number " + number + " is titled " + NPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + NPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", NPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
                     case 'G':
                         print ("Item number " + number + " is titled " + GPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + GPost['items'][int(number)-1]['current_bid'])
+                        cleanedq = Cleanquery.sub("", GPost['items'][int(number)-1]['title'])
+                        Fulllink = cccprice + str(cleanedq)
+                        webbrowser.get('edge').open(Fulllink)
         except:
             print("Invalid input. Please try again.")
-        
-Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: \n")
+
+Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
 while Redirectto != "No":
     if (Redirectto == "Sacramento" or Redirectto == "sacramento" or Redirectto == 's' or Redirectto == 'S'):
         print ("The first auction gallery in Sacramento is a " + (Sjson['auctions']['1']['title']))
@@ -120,7 +143,7 @@ while Redirectto != "No":
         break
     elif (Redirectto == "Elk Grove" or Redirectto == "elk grove" or Redirectto == "elkgrove" or Redirectto == "Elkgrove" or Redirectto == 'e' or Redirectto == 'E'):
         print ("The first auction gallery in Elk Grove is a " + (Ejson['auctions'][0]['title']))
-        print ("There are " + Ejson['auctions']['1']['item_count'] + " items in this gallery.")
+        print ("There are " + Ejson['auctions'][1]['item_count'] + " items in this gallery.")
         Itemcheck("E")
         
     elif (Redirectto == "Rancho Cordova" or Redirectto == "rancho cordova" or Redirectto == "ranchocordova" or Redirectto == "Ranchocordova" or Redirectto == 'r' or Redirectto == 'R'):
@@ -146,3 +169,4 @@ while Redirectto != "No":
         print("Keyword not valid or recognized. Please type again:")
     Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
 
+#
