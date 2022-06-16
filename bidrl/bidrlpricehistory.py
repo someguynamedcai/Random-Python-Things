646bbd3c8fa203a337da00c03d975331919c48c1
Eric Cai <someguynamedcai@gmail.com>
2022-06-15 22:49:00 -0700

    Functioning prompt and loop
    It is now looping correctly and not causing any errors. Finding out the price history via camelcamelcamel is next.

--- bidrl/bidrlpricehistory.py
+++ bidrl/bidrlpricehistory.py
@@ -79,80 +79,69 @@ CItem1 = CPost['items'][0]['title']
 NItem1 = NPost['items'][0]['title']
 GItem1 = GPost['items'][0]['title']
 
-def Itemcheck(location,number):
+def Itemcheck(location):
     while (True):
-        if number == "exit" or number == "Exit":
-            break
-        else:
-            match location:
-                case 'S':
-                    print ("Item number " + number + " is titled " + SPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + SPost['items'][int(number)-1]['current_bid'])
-                case 'E':
-                    print ("Item number " + number + " is titled " + EPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + EPost['items'][int(number)-1]['current_bid'])
-                case 'R':
-                    print ("Item number " + number + " is titled " + RPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + RPost['items'][int(number)-1]['current_bid'])
-                case 'C':
-                    print ("Item number " + number + " is titled " + CPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + CPost['items'][int(number)-1]['current_bid'])
-                case 'N':
-                    print ("Item number " + number + " is titled " + NPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + NPost['items'][int(number)-1]['current_bid'])
-                case 'G':
-                    print ("Item number " + number + " is titled " + GPost['items'][int(number)-1]['title'])
-                    print ("The current price of item number " + number + " is " + GPost['items'][int(number)-1]['current_bid'])            
         try:
-            number = input("Type in another number or type exit to go back to the previous prompt (1-12 only): ")
-            number = int(number)
+            number = input("Type in a number or type exit to go back to the previous prompt (1-12 only): ")
+            number = str(number)
+            if number == "exit" or number == "Exit":
+                return 
+            else:
+                match location:
+                    case 'S':
+                        print ("Item number " + number + " is titled " + SPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + SPost['items'][int(number)-1]['current_bid'])
+                    case 'E':
+                        print ("Item number " + number + " is titled " + EPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + EPost['items'][int(number)-1]['current_bid'])
+                    case 'R':
+                        print ("Item number " + number + " is titled " + RPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + RPost['items'][int(number)-1]['current_bid'])
+                    case 'C':
+                        print ("Item number " + number + " is titled " + CPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + CPost['items'][int(number)-1]['current_bid'])
+                    case 'N':
+                        print ("Item number " + number + " is titled " + NPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + NPost['items'][int(number)-1]['current_bid'])
+                    case 'G':
+                        print ("Item number " + number + " is titled " + GPost['items'][int(number)-1]['title'])
+                        print ("The current price of item number " + number + " is $" + GPost['items'][int(number)-1]['current_bid'])
         except:
-            print("Please use numeric digits.")
-            
-
-def Numfind():
-    Itemnumber = input("Which item would you like to view? (Only 1-12 right now): ")
-    return str(Itemnumber)
-
-Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
+            print("Invalid input. Please try again.")
+        
+Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: \n")
 while Redirectto != "No":
     if (Redirectto == "Sacramento" or Redirectto == "sacramento" or Redirectto == 's' or Redirectto == 'S'):
         print ("The first auction gallery in Sacramento is a " + (Sjson['auctions']['1']['title']))
         print ("There are " + Sjson['auctions']['1']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("S",itemnumber)
+        Itemcheck("S")
     elif (Redirectto == "Exit" or Redirectto == "exit"):
         print ("Exiting program")
         break
     elif (Redirectto == "Elk Grove" or Redirectto == "elk grove" or Redirectto == "elkgrove" or Redirectto == "Elkgrove" or Redirectto == 'e' or Redirectto == 'E'):
         print ("The first auction gallery in Elk Grove is a " + (Ejson['auctions'][0]['title']))
-        print ("There are " + Ejson['auctions']['1']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("E",itemnumber)
+        print ("There are " + Ejson['auctions']['1']['item_count'] + " items in this gallery.")
+        Itemcheck("E")
         
     elif (Redirectto == "Rancho Cordova" or Redirectto == "rancho cordova" or Redirectto == "ranchocordova" or Redirectto == "Ranchocordova" or Redirectto == 'r' or Redirectto == 'R'):
         print ("The first auction gallery in Rancho Cordova is a " + (Rjson['auctions']['1']['title']))
-        print ("There are " + Rjson['auctions']['1']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("R",itemnumber)
+        print ("There are " + Rjson['auctions']['1']['item_count'] + " items in this gallery.")
+        Itemcheck("R")
         
     elif (Redirectto == "Citrus Heights" or Redirectto == "citrus heights" or Redirectto == "citrusheights" or Redirectto == "Citrusheights" or Redirectto == 'C' or Redirectto == 'c'):
         print ("The first auction gallery in Citrus Heights is a " + (Cjson['auctions']['1']['title']))
-        print ("There are " + Gjson['auctions']['2']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("C",itemnumber)
+        print ("There are " + Gjson['auctions']['2']['item_count'] + " items in this gallery.")
+        Itemcheck("C")
         
     elif (Redirectto == "Natomas" or Redirectto == "natomas" or Redirectto == 'n' or Redirectto == 'N'):
         print ("The first auction gallery in Natomas is a " + (Njson['auctions']['1']['title']))
-        print ("There are " + Njson['auctions']['1']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("N",itemnumber)
+        print ("There are " + Njson['auctions']['1']['item_count'] + " items in this gallery.")
+        Itemcheck("N")
                
     elif (Redirectto == "Galt" or Redirectto == "galt" or Redirectto == 'g' or Redirectto == 'G'):
         print ("The first auction gallery in Galt is a " + (Gjson['auctions']['2']['title']))
-        print ("There are " + Gjson['auctions']['2']['item_count'] + " items in this gallery")
-        itemnumber = Numfind()
-        Itemcheck("G",itemnumber)
+        print ("There are " + Gjson['auctions']['2']['item_count'] + " items in this gallery.")
+        Itemcheck("G")
     else:
         print("Keyword not valid or recognized. Please type again:")
     Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
