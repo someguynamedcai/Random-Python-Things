df136de8c7ce1b72c83d55ea2a0598d85645aadb
Eric Cai <someguynamedcai@gmail.com>
2022-06-19 12:22:52 -0700

    Trying to add text file that will hopefully keep track of all bids I have made and paid for

--- bidrl/totalpricebidrl.py
+++ bidrl/totalpricebidrl.py
@@ -1,17 +1,27 @@
+#Testing how to find the total price for an item at bidrl.com
+total = 0
 price = 1
+counter = 1
+allitems = 0
+file1 = open("TotalPaidBidrl.txt","w")
 while (price != 0):
+    try:
+        price = input("What is the price? (type exit to close program): ")
 
-    price = input("What is the price? (type exit to close program): ")
+        if (price == "exit" or price == "Exit"):
+            print ("Program End")
+            break
 
-    if (price == "exit" or price == "Exit"):
-        print ("Program End")
-        break
-
-    tax = float(price) * (.0825)
-    premium = float(price) * (.13)
-
-    print ("\nThe tax (8.25%) paid is " + str(tax))
-    print ("The premium (13%) paid is " + str(premium))
-    print ("The total paid is " + (str(float(price) + tax + premium)) + "\n")
-
+        tax = float(price) * (.0825)
+        premium = float(price) * (.13)
+        total = float(price) + tax + premium
+        allitems = allitems + total
+        print ("\nThe tax (8.25%) paid is " + str(tax) + ".")
+        print ("The premium (13%) paid is " + str(premium) + ".")
+        print ("The total paid is " + (str(total) + "."))
+        print ("For all " + str(counter) + " items paid it is " + str(allitems) + ".\n")
+        counter += 1
+        file1.write(str(total))
+    except:
+        print ("Invalid input. Try again")
 
