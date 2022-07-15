46d609cc68385317704c6c78f8f45155789addb7
Eric Cai <someguynamedcai@gmail.com>
2022-07-15 08:34:19 -0700

    Added text and stops recording when done.

--- mouseauto.py
+++ mouseauto.py
@@ -1,6 +1,7 @@
 #Attempt at mouse automation
 import mouse
 
+print ("Mouse recording started. To end the recording press the right mouse button")
 mouse.on_click(lambda: print("Left Button clicked."))
 events = mouse.record()
 
@@ -9,5 +10,6 @@ loop = int(input("How many times would you like to repeat these actions: "))
 
 for count in range(loop):
     mouse.play(events[:-1])
-
-print ("Completed loop.")
+    print ("Completed loop " + count + ".")
+mouse.unhook_all()
+done = input("Completed loop.")
