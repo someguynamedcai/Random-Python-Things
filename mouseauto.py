ffeddb9d0bb57a4b7ea62ac7b823afa354eb940f
Eric Cai <someguynamedcai@gmail.com>
2022-07-16 15:12:49 -0700

    Fixed loop
    It can now loop correctly multiple times.

--- mouseauto.py
+++ mouseauto.py
@@ -1,7 +1,7 @@
 #Attempt at mouse automation
 import mouse
 
-print ("Mouse recording started. To end the recording press the right mouse button")
+print ("Mouse recording started. To end the recording press the right mouse button inside the box.")
 mouse.on_click(lambda: print("Left Button clicked."))
 events = mouse.record()
 
@@ -10,6 +10,6 @@ loop = int(input("How many times would you like to repeat these actions: "))
 
 for count in range(loop):
     mouse.play(events[:-1])
-    print ("Completed loop " + count + ".")
+    print ("Completed loop " + str(count + 1) + ".")
 mouse.unhook_all()
-done = input("Completed loop.")
+done = input("Completed mouse playback.")
