4402d4e318cc2cfa986489d946b99631c455dfbe
Eric Cai <someguynamedcai@gmail.com>
2022-07-16 15:32:03 -0700

    Changed to indicate profit; added date text
    I don't know what I was doing originally but it made no sense. I changed it so now it can indicate how much net gain or loss there is when cashing out.

--- price.py
+++ price.py
@@ -1,4 +1,6 @@
 from urllib.request import Request, urlopen
+import datetime
+import time
 
 req = Request('https://www.google.com/search?&q=bitcoin', headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
@@ -7,24 +9,26 @@ html = webpage.decode("utf-8")
 #price = urllib.request.urlopen("https://www.google.com/search?&q=bitcoin")
 #html_bytes = price.read()
 pricefind = html.find("United States Dollar")
-print (pricefind)
 currentprice = html[pricefind-10:pricefind-1]
-print (currentprice)
 currentprice = float(currentprice.replace(',',''))
 buyin = None
 buyinprice = None
+currenttime = datetime.datetime.now()
+currentunixtime = time.time()
+
+print ("Today is " + str(currenttime.date()) + ".\nThe time is " + str(currenttime.time()) + "\n")
 
 while buyin == None:
-    buyin = float(input("How much did bitcoin you buy? "))
+    buyin = float(input("How much did bitcoin you buy: "))
 while buyinprice == None:
-    buyinprice = float(input("How much was it worth? "))
+    buyinprice = float(input("How much was one bitcoin worth at the time: "))
     
+moneyin = buyin * buyinprice
 difference = ((currentprice/buyinprice) - 1)
-print ("The current price is " + str(currentprice) + ".")
+print ("The current price is $" + str(currentprice) + ".")
 if difference > 0:
     print ("Today it has increased by " + str(difference*100) + "%.")
 else:
-    print ("Today it has decreased by " + str(1 - difference*100) + "%.")
+    print ("Today it has decreased by " + str(1-difference*100) + "%.")
 
-print ("Your buy-in is currently at " + str((buyin + (buyin*(difference)))) + ".")
-
+print ("Selling all of your bitcoin now will net you a profit of " + (str((buyin * currentprice)-moneyin)) + " dollars.")
