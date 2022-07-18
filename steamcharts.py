7d3bd97bcd77a7f19a380435db382898e0adcba5
Eric Cai <someguynamedcai@gmail.com>
2022-07-18 07:58:53 -0700

    Changed to display text not open a webpage
    Changed it so now it web scrapes information rather than open up a webpage of the query. Bug fixing required.

--- steamcharts.py
+++ steamcharts.py
@@ -1,5 +1,6 @@
-import webbrowser
-
+from urllib.request import Request, urlopen
+gameurl = ""
+playercount = 0
 
 while (True):
     game = (input("Type in an game to see Steamcharts stats or type Exit to exit: "))
@@ -12,7 +13,22 @@ while (True):
         query = (query + num + "+")
     query = query.rstrip(query[-1])
     print ("Searching for the game " + game + " in Steamcharts.")
-    webbrowser.open('https://steamcharts.com/search/?q='+ query)
+    gameurl = ("https://steamcharts.com/search/?q=" + query)
+    req = Request(gameurl, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+    html = webpage.decode("utf-8")
+    playerfind = html.find("right num")
+    playercount = html[playerfind+11:playerfind+17]
+    print (playercount)
+    print (playercount.isalpha())
+    if (playercount.isalpha() == True):
+        print ("Steamcharts could not find any results for your search. Please try again.")
+    elif playercount == "0":
+        print ("Steamcharts sees there are zero players. You may have typed your query wrong in some cases.") 
+    else:   
+        print ("Steamcharts indicates there are " + playercount + " players currently playing the game right now.")
+
+
 
 
 
