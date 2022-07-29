9583f903fdc82237bf25a8c65fc2fcb396161a96
Eric Cai <someguynamedcai@gmail.com>
2022-07-29 08:04:55 -0700

    Restructured
    Restrucutured so information comes up much faster than before. Rather than waiting for all the information to load it will immediately print out the text for the location's information when it loads.

--- bidrl/bidrl.py
+++ bidrl/bidrl.py
@@ -13,7 +13,13 @@ currentunixtime = time.time()
 print ("Today is " + str(currenttime.date()) + ".\nThe time is " + str(currenttime.time()) + "\n")
 
 # read all data
-
+def Oneitem(location,ID):
+    if (location == "R"):
+        RPost = requests.post("https://www.bidrl.com/api/ItemData", data = {"item_id": ID,}).json()
+        RItemtime = float(RPost['end_time']) + 7200
+        Rtimeleft = RItemtime - currentunixtime
+        print ("This item will close in " + str(datetime.timedelta(seconds = Rtimeleft)))
+    
 #Spage = requests.get("https://www.bidrl.com/api/landingPage/sacramento-2")
 Spage = requests.get("https://www.bidrl.com/api/landingPage/cesar-lua-2")
 Sjson = Spage.json()
@@ -48,101 +54,13 @@ for Sauctions in Sjson['auctions'].keys():
         S_Id = Sjson['auctions'][Sauctions]['id']
         break
     
-#Elk Grove Json page is not a dictionary but a list for some reason
-for Eauctions in range(len(Ejson['auctions'])):
-    if (Ejson['auctions'][Eauctions]['status'] == "open"):
-        E_Id = Ejson['auctions'][Eauctions]['id']
-        break
-    
-for Rauctions in Rjson['auctions'].keys():   
-    if Rjson['auctions'][Rauctions]['status'] == "open":
-        R_Id = Rjson['auctions'][Rauctions]['id']
-        break
-        
-for Cauctions in Cjson['auctions'].keys():
-    if Cjson['auctions'][Cauctions]['status'] == "open":
-        C_Id = Cjson['auctions'][Cauctions]['id']
-        break
-
-for Nauctions in Njson['auctions'].keys():    
-    if Njson['auctions'][Nauctions]['status'] == "open":
-        N_Id = Njson['auctions'][Nauctions]['id']
-        break
-
-for Gauctions in Gjson['auctions'].keys():    
-    if Gjson['auctions'][Gauctions]['status'] == "open":
-        G_Id = Gjson['auctions'][Gauctions]['id']
-        break
-
-for ESauctions in range(len(ESjson['auctions'])):
-    if (ESjson['auctions'][str(ESauctions+1)]['status'] == "open"):
-        ES_Id = ESjson['auctions'][str(ESauctions+1)]['id']
-        break
-   
-#Putting it into format for POST to getitems json page
 SItems = {"auction_id": S_Id,}
-EItems = {"auction_id": E_Id,}
-RItems = {"auction_id": R_Id,}
-CItems = {"auction_id": C_Id,}
-NItems = {"auction_id": N_Id,}
-GItems = {"auction_id": G_Id,}
-ESItems = {"auction_id": ES_Id,}
-
-#Retrieves item information from most recent gallery
 SPost = requests.post(getitems, data = SItems).json()
-EPost = requests.post(getitems, data = EItems).json()
-RPost = requests.post(getitems, data = RItems).json()
-CPost = requests.post(getitems, data = CItems).json()
-NPost = requests.post(getitems, data = NItems).json()
-GPost = requests.post(getitems, data = GItems).json()
-ESPost = requests.post(getitems, data = ESItems).json()
-
-
 SItem1 = SPost['items'][0]['title']
-EItem1 = EPost['items'][0]['title']
-RItem1 = RPost['items'][0]['title']
-CItem1 = CPost['items'][0]['title']
-NItem1 = NPost['items'][0]['title']
-GItem1 = GPost['items'][0]['title']
-ESItem1 = ESPost['items'][0]['title']
-
-
-
-#For some reason Galt json starts out with 2
 Stimes = Sjson['auctions']['2']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-Etimes = Ejson['auctions'][0]['info_div'].replace("<b>"," ").replace("</b>","").replace("<br>","").replace("<br />","")
-Rtimes = Rjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-Ctimes = Cjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-Ntimes = Njson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-Gtimes = Gjson['auctions']['3']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-EStimes = ESjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
-
-
-#Calculating the time left before the first item closes
 SItemtime = float(SPost['items'][0]['ends']) + 7200
 Stimeleft = SItemtime - currentunixtime
-EItemtime = float(EPost['items'][0]['ends']) + 7200
-Etimeleft = EItemtime - currentunixtime
-RItemtime = float(RPost['items'][0]['ends']) + 7200
-Rtimeleft = RItemtime - currentunixtime
-CItemtime = float(CPost['items'][0]['ends']) + 7200
-Ctimeleft = CItemtime - currentunixtime
-NItemtime = float(NPost['items'][0]['ends']) + 7200
-Ntimeleft = NItemtime - currentunixtime
-GItemtime = float(GPost['items'][0]['ends']) + 7200
-Gtimeleft = GItemtime - currentunixtime
-ESItemtime = float(ESPost['items'][0]['ends']) + 7200
-EStimeleft = ESItemtime - currentunixtime
 
-#Seems to only be for the Rancho Cordova location
-def Oneitem(location,ID):
-    if (location == "R"):
-        RPost = requests.post("https://www.bidrl.com/api/ItemData", data = {"item_id": ID,}).json()
-        RItemtime = float(RPost['end_time']) + 7200
-        Rtimeleft = RItemtime - currentunixtime
-        print ("This item will close in " + str(datetime.timedelta(seconds = Rtimeleft)))        
-
-#Information on the most recent auction and when the first item closes
 if Stimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Sacramento is a " + (Sjson['auctions']['2']['title']))
     print ("The first item in this gallery is titled " + SItem1 + ".")
@@ -161,7 +79,18 @@ else:
     print (Stimes + "\nThe link to the auction is \nhttps://www.bidrl.com/auction/" +  Sjson['auctions']['3']['auction_id_slug'] + "\n")
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
     
+#Elk Grove Json page is not a dictionary but a list for some reason
+for Eauctions in range(len(Ejson['auctions'])):
+    if (Ejson['auctions'][Eauctions]['status'] == "open"):
+        E_Id = Ejson['auctions'][Eauctions]['id']
+        break
 
+EItems = {"auction_id": E_Id,}
+EPost = requests.post(getitems, data = EItems).json()
+EItem1 = EPost['items'][0]['title']
+Etimes = Ejson['auctions'][0]['info_div'].replace("<b>"," ").replace("</b>","").replace("<br>","").replace("<br />","")
+EItemtime = float(EPost['items'][0]['ends']) + 7200
+Etimeleft = EItemtime - currentunixtime
 if Etimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Elk Grove is a " + (Ejson['auctions'][0]['title']))
     print ("The first item in this gallery is titled " + EItem1 + ".")
@@ -179,7 +108,18 @@ else:
     print ("An auction has recently closed. The next auction gallery in Elk Grove is a " + (Ejson['auctions'][1]['title']))
     print (Stimes + "\nThe link to the auction is \nhttps://www.bidrl.com/auction/" +  Ejson['auctions'][1]['auction_id_slug']  + "\n")
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
-    
+
+for Rauctions in Rjson['auctions'].keys():   
+    if Rjson['auctions'][Rauctions]['status'] == "open":
+        R_Id = Rjson['auctions'][Rauctions]['id']
+        break
+
+RItems = {"auction_id": R_Id,}
+RPost = requests.post(getitems, data = RItems).json()
+RItem1 = RPost['items'][0]['title']
+Rtimes = Rjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
+RItemtime = float(RPost['items'][0]['ends']) + 7200
+Rtimeleft = RItemtime - currentunixtime
 if Rtimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Rancho Cordova is a " + (Rjson['auctions']['1']['title']))
     print ("The first item in this gallery is titled " + RItem1 + ".")
@@ -201,7 +141,18 @@ else:
     print (Rtimes + "\nThe link to the auction is \nhttps://www.bidrl.com/auction/" +  Rjson['auctions']['2']['auction_id_slug'] + "\n")
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
 
-    
+        
+for Cauctions in Cjson['auctions'].keys():
+    if Cjson['auctions'][Cauctions]['status'] == "open":
+        C_Id = Cjson['auctions'][Cauctions]['id']
+        break
+
+CItems = {"auction_id": C_Id,}
+CPost = requests.post(getitems, data = CItems).json()
+CItem1 = CPost['items'][0]['title']
+Ctimes = Cjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
+CItemtime = float(CPost['items'][0]['ends']) + 7200
+Ctimeleft = CItemtime - currentunixtime
 if Ctimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Citrus Heights is a " + (Cjson['auctions']['1']['title']))
     print ("The first item in this gallery is titled " + CItem1 + ".")
@@ -221,6 +172,17 @@ else:
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
 
 
+for Nauctions in Njson['auctions'].keys():    
+    if Njson['auctions'][Nauctions]['status'] == "open":
+        N_Id = Njson['auctions'][Nauctions]['id']
+        break
+
+NItems = {"auction_id": N_Id,}
+NPost = requests.post(getitems, data = NItems).json()
+NItem1 = NPost['items'][0]['title']
+Ntimes = Njson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
+NItemtime = float(NPost['items'][0]['ends']) + 7200
+Ntimeleft = NItemtime - currentunixtime
 if Ntimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Natomas is a " + (Njson['auctions']['1']['title']))
     print ("The first item in this gallery is titled " + NItem1 + ".")
@@ -239,7 +201,16 @@ else:
     print (Ntimes + "\nThe link to the auction is \nhttps://www.bidrl.com/auction/" +  Njson['auctions']['2']['auction_id_slug'] + "\n")
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
 
-
+for Gauctions in Gjson['auctions'].keys():    
+    if Gjson['auctions'][Gauctions]['status'] == "open":
+        G_Id = Gjson['auctions'][Gauctions]['id']
+        break
+GItems = {"auction_id": G_Id,}
+GPost = requests.post(getitems, data = GItems).json()
+GItem1 = GPost['items'][0]['title']
+Gtimes = Gjson['auctions']['3']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
+GItemtime = float(GPost['items'][0]['ends']) + 7200
+Gtimeleft = GItemtime - currentunixtime
 if Gtimes.find("Closing Time") != -1 or Gtimes.find("First Item Closes") != -1:
     print ("The first auction gallery in Galt is a " + (Gjson['auctions']['3']['title']))
     print ("The first item in this gallery is titled " + GItem1 + ".")
@@ -257,6 +228,29 @@ else:
     print ("An auction has recently closed. The next auction gallery in Galt is a " + (Gjson['auctions']['4']['title']))
     print (Gtimes + "\nThe link to the auction is \nhttps://www.bidrl.com/auction/" +  Gjson['auctions']['4']['auction_id_slug'] + "\n")
     print ("------------------------------------------------------------------------------------------------------------------------------"  + "\n")
+    
+for ESauctions in range(len(ESjson['auctions'])):
+    if (ESjson['auctions'][str(ESauctions+1)]['status'] == "open"):
+        ES_Id = ESjson['auctions'][str(ESauctions+1)]['id']
+        break
+   
+#Putting it into format for POST to getitems json page
+ESItems = {"auction_id": ES_Id,}
+
+#Retrieves item information from most recent gallery
+ESPost = requests.post(getitems, data = ESItems).json()
+
+ESItem1 = ESPost['items'][0]['title']
+
+#For some reason Galt json starts out with 2
+EStimes = ESjson['auctions']['1']['info_div'].replace("<b>","").replace("</b>"," ").replace("<br>","").replace("<br />"," ")
+
+#Calculating the time left before the first item closes
+ESItemtime = float(ESPost['items'][0]['ends']) + 7200
+EStimeleft = ESItemtime - currentunixtime
+
+#Information on the most recent auction and when the first item closes
+
 
 if EStimes.find("First Item Closes") != -1:
     print ("The first auction gallery in East Sacramento is a " + (ESjson['auctions']['1']['title']))
@@ -326,9 +320,3 @@ while Redirectto != "No":
         Redirectto = input("Please type a location to go to or type in 'Exit' to exit: ")
     except KeyError:
         print ("There has been an error in opening up the gallery. It will be fixed shortly.")
-
-
-#6/8/2022 Added more options to redirect to webpages
-#6/7/2022 Added Galt to locations it finds info on.
-#6/5/2022 Added prompt and redesigned search to pass more test cases
-#6/1/2022 JSON Page switched to name cesar-lua for some reason for Sacramento json page
