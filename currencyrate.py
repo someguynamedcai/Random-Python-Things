c5f0cae1cb734ffa2981b4f5ded6238da4c506e5
Eric Cai <someguynamedcai@gmail.com>
2023-02-14 09:59:26 -0800

    Update currencyrate.py
    Added input

--- currencyrate.py
+++ currencyrate.py
@@ -9,7 +9,12 @@ html = webpage.decode("utf-8")
 #price = urllib.request.urlopen("https://www.google.com/search?&q=bitcoin")
 #html_bytes = price.read()
 pricefind = html.find("United States Dollar")
-currentprice = html[pricefind-5:pricefind+20]
+currentprice = html[pricefind-5:pricefind-1]
 
-print ("The current conversion of Euros to USD is " + currentprice + "s.")
-text = input()
+print ("The current conversion of Euros to USD is " + currentprice + ".")
+while (True):
+    text = input("How much would you like to convert:")
+    if text.isdigit():
+        print ("That is " + str(float(currentprice)*float(text)) + " Euros.")
+else:
+    print("Invalid input. Please enter a number.")
