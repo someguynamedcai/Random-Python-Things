08a79c1a645e3167f95e4fb031baa1490aaf280e
Eric Cai <someguynamedcai@gmail.com>
2023-06-18 22:14:01 -0700

    Removed Sacramento; changed search to keys
    Removed Sacramento as that location is not being used anymore. Changed several searches to keys to make search functional.

--- bidrl/bidrlpricehistory.py
+++ bidrl/bidrlpricehistory.py
@@ -6,11 +6,12 @@ import webbrowser
 import re
 import time
 
-print ("The following locations are: Sacramento, Elk Grove, Rancho Cordova, Citrus Heights, Natomas, Galt, and East Sacramento. \nPlease wait while the auction information is being loaded.")
+print ("The following locations are: Elk Grove, Rancho Cordova, Citrus Heights, Natomas, Galt, and East Sacramento. \nPlease wait while the auction information is being loaded.")
 
 #Spage = requests.get("https://www.bidrl.com/api/landingPage/sacramento-2")
-Spage = requests.get("https://www.bidrl.com/api/landingPage/cesar-lua-2")
-Sjson = Spage.json()
+#Spage = requests.get("https://www.bidrl.com/api/landingPage/cesar-lua-2")
+#Spage = requests.get("https://www.bidrl.com/api/landingPage/sacramento")
+#Sjson = Spage.json()
 Epage = requests.get("https://www.bidrl.com/api/landingPage/elk-grove-8")
 Ejson = Epage.json()
 Rpage = requests.get("https://www.bidrl.com/api/landingPage/rancho-cordova-34")
@@ -33,8 +34,8 @@ auctionlink = "bidrl.com/auction/"
 edge_path="C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
 webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
 
-#Finding out the auction id of most recent gallery" that is not closed
-try:
+'''#Finding out the auction id of most recent gallery" that is not closed
+#try:
     for Sauctions in Sjson['auctions'].keys():
         if Sjson['auctions'][Sauctions]['status'] == "open":
             S_Id = Sjson['auctions'][Sauctions]['id']
@@ -42,10 +43,10 @@ try:
             break
 except (NameError, AttributeError):
         print ("There are no open auctions in Sacramento as of this moment.\n")
-        
+'''        
 #Elk Grove Json page is not a dictionary but a list for some reason
 try:
-    for Eauctions in range(len(Ejson['auctions'])):
+    for Eauctions in Ejson['auctions'].keys():
         if (Ejson['auctions'][Eauctions]['status'] == "open"):
             E_Id = Ejson['auctions'][Eauctions]['id']
             break
@@ -93,7 +94,7 @@ except (NameError, AttributeError):
         print ("There are no open auctions in East Sacramento as of this moment.\n")
         
 try:
-    for ROauctions in range(len(ROjson['auctions'])):
+    for ROauctions in ROjson['auctions'].keys():
         if ROjson['auctions'][ROauctions]['status'] == "open":
             RO_Id = ROjson['auctions'][ROauctions]['id']
             ROfirst = ROauctions
@@ -101,7 +102,7 @@ try:
 except (NameError, AttributeError):
         print ("There are no open auctions in Roseville as of this moment.\n")
 #Putting it into format for POST to getitems json page
-SItems = {"auction_id": S_Id, "filters[perpage]": Sjson['auctions'][Sauctions]['item_count'] }
+#SItems = {"auction_id": S_Id, "filters[perpage]": Sjson['auctions'][Sauctions]['item_count'] }
 EItems = {"auction_id": E_Id, "filters[perpage]": Ejson['auctions'][Eauctions]['item_count']}
 RItems = {"auction_id": R_Id, "filters[perpage]": Rjson['auctions'][Rauctions]['item_count']}
 CItems = {"auction_id": C_Id, "filters[perpage]": Cjson['auctions'][Cauctions]['item_count']}
@@ -111,7 +112,7 @@ ESItems = {"auction_id": ES_Id, "filters[perpage]": ESjson['auctions'][ESauction
 
 
 #Retrieves item information from most recent gallery"
-SPost = requests.post(getitems, data = SItems).json()
+#SPost = requests.post(getitems, data = SItems).json()
 EPost = requests.post(getitems, data = EItems).json()
 RPost = requests.post(getitems, data = RItems).json()
 CPost = requests.post(getitems, data = CItems).json()
@@ -121,7 +122,7 @@ ESPost = requests.post(getitems, data = ESItems).json()
 
 
 
-SItem1 = SPost['items'][0]['title']
+#SItem1 = SPost['items'][0]['title']
 EItem1 = EPost['items'][0]['title']
 RItem1 = RPost['items'][0]['title']
 CItem1 = CPost['items'][0]['title']
@@ -143,19 +144,7 @@ def Itemcheck(location):
                 print ("Exiting current location.")
                 return 
             else:
-                match location:
-                    case 'S':
-                        print ("Item number " + number + " is titled " + SPost['items'][int(number)-1]['title'])
-                        print ("The current price of item number " + number + " is $" + SPost['items'][int(number)-1]['current_bid'])
-                        print ("The total price calculated with the 8.25% tax and 13% buyer's premium is $" + str(float(SPost['items'][0]['current_bid']) * .0825 + float(SPost['items'][0]['current_bid']) + float(SPost['items'][0]['current_bid']) * .13))
-                        cleanedq = Cleanquery.sub("", SPost['items'][int(number)-1]['title'])
-                        Fulllink = cccprice + str(cleanedq)
-                        Fullauctionlink = auctionlink + Sjson['auctions'][Sauctions]['id'] + "/item/" + SPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
-                        time.sleep(2)
-                        webbrowser.get('edge').open(Fulllink)
-                        webbrowser.get('edge').open(Fullauctionlink)                                                                               
-
+                match location:       
                     case 'E':
                         print ("Item number " + number + " is titled " + EPost['items'][int(number)-1]['title'])
                         print ("The current price of item number " + number + " is $" + EPost['items'][int(number)-1]['current_bid'])
@@ -163,7 +152,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", EPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + Ejson['auctions'][Eauctions]['id'] + "/item/" + EPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)                                                                               
@@ -175,7 +164,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", RPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + Rjson['auctions'][Rauctions]['id'] + "/item/" + RPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -187,7 +176,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", CPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + Cjson['auctions'][Cauctions]['id'] + "/item/" + CPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -199,7 +188,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", NPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + Njson['auctions'][Nauctions]['id'] + "/item/" + NPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -211,7 +200,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", GPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + Gjson['auctions'][Gauctions]['id'] + "/item/" + GPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -223,7 +212,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", ESPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + ESjson['auctions'][ESauctions]['id'] + "/item/" + ESPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -235,7 +224,7 @@ def Itemcheck(location):
                         cleanedq = Cleanquery.sub("", ROPost['items'][int(number)-1]['title'])
                         Fulllink = cccprice + str(cleanedq)
                         Fullauctionlink = auctionlink + ROjson['auctions'][ROauctions]['id'] + "/item/" + ROPost['items'][int(number)-1]['id']                                                                     
-                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds.")
+                        print ("A camelcamelcamel page and a page to the item will open in 2 seconds. Item may not be related to item in gallery.")
                         time.sleep(2)
                         webbrowser.get('edge').open(Fulllink)
                         webbrowser.get('edge').open(Fullauctionlink)
@@ -246,45 +235,46 @@ def Itemcheck(location):
 
 Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
 while Redirectto != "No":
-    if (Redirectto == "Sacramento" or Redirectto == "sacramento" or Redirectto == 's' or Redirectto == 'S'):
+    '''if (Redirectto == "Sacramento" or Redirectto == "sacramento" or Redirectto == 's' or Redirectto == 'S'):
         print ("The first auction gallery in Sacramento is a " + (Sjson['auctions'][Sauctions]['title']))
         print ("There are " + Sjson['auctions'][Sauctions]['item_count'] + " items in this gallery")
         Itemcheck("S")
-    elif (Redirectto == "Exit" or Redirectto == "exit"):
+    '''
+    if (Redirectto == "Exit" or Redirectto == "exit"):
         print ("Exiting program")
         break
     elif (Redirectto == "Elk Grove" or Redirectto == "elk grove" or Redirectto == "elkgrove" or Redirectto == "Elkgrove" or Redirectto == 'e' or Redirectto == 'E'):
-        print ("The first auction gallery in Elk Grove is a " + (Ejson['auctions'][0]['title']))
+        print ("The first auction gallery in Elk Grove is a " + (Ejson['auctions'][Eauctions]['title']))
         print ("There are " + Ejson['auctions'][Eauctions]['item_count'] + " items in this gallery.")
         Itemcheck("E")
         
     elif (Redirectto == "Rancho Cordova" or Redirectto == "rancho cordova" or Redirectto == "ranchocordova" or Redirectto == "Ranchocordova" or Redirectto == 'r' or Redirectto == 'R'):
-        print ("The first auction gallery in Rancho Cordova is a " + (Rjson['auctions']['1']['title']))
+        print ("The first auction gallery in Rancho Cordova is a " + (Rjson['auctions'][Rauctions]['title']))
         print ("There are " + Rjson['auctions'][Rauctions]['item_count'] + " items in this gallery.")
         Itemcheck("R")
         
     elif (Redirectto == "Citrus Heights" or Redirectto == "citrus heights" or Redirectto == "citrusheights" or Redirectto == "Citrusheights" or Redirectto == 'C' or Redirectto == 'c'):
-        print ("The first auction gallery in Citrus Heights is a " + (Cjson['auctions']['1']['title']))
+        print ("The first auction gallery in Citrus Heights is a " + (Cjson['auctions'][Cauctions]['title']))
         print ("There are " + Cjson['auctions'][Cauctions]['item_count'] + " items in this gallery.")
         Itemcheck("C")
         
     elif (Redirectto == "Natomas" or Redirectto == "natomas" or Redirectto == 'n' or Redirectto == 'N'):
-        print ("The first auction gallery in Natomas is a " + (Njson['auctions']['1']['title']))
+        print ("The first auction gallery in Natomas is a " + (Njson['auctions'][Nauctions]['title']))
         print ("There are " + Njson['auctions'][Nauctions]['item_count'] + " items in this gallery.")
         Itemcheck("N")
                
     elif (Redirectto == "Galt" or Redirectto == "galt" or Redirectto == 'g' or Redirectto == 'G'):
-        print ("The first auction gallery in Galt is a " + (Gjson['auctions']['3']['title']))
+        print ("The first auction gallery in Galt is a " + (Gjson['auctions'][Gauctions]['title']))
         print ("There are " + Gjson['auctions'][Gauctions]['item_count'] + " items in this gallery.")
         Itemcheck("G")
     elif (Redirectto == "East Sacramento" or Redirectto == "east sacramento" or Redirectto == 'es' or Redirectto == 'ES' or Redirectto == 'Es' or Redirectto == 'east' or Redirectto == 'East'):
-        print ("The first auction gallery in East Sacramento is a " + (ESjson['auctions']['1']['title']))
+        print ("The first auction gallery in East Sacramento is a " + (ESjson['auctions'][ESauctions]['title']))
         print ("There are " + ESjson['auctions'][ESauctions]['item_count'] + " items in this gallery.")
         Itemcheck("ES")
     elif (Redirectto == "Roseville" or Redirectto == "roseville" or Redirectto == 'ro' or Redirectto == 'RO' or Redirectto == 'Ro' or Redirectto == 'rose' or Redirectto == 'Rose'):
-        print ("The first auction gallery in Roseville is a " + (ROjson['auctions']['1']['title']))
+        print ("The first auction gallery in Roseville is a " + (ROjson['auctions'][ROauctions]['title']))
         print ("There are " + ROjson['auctions'][ROauctoins]['item_count'] + " items in this gallery.")
     else:
         print("Keyword not valid or recognized. Please type again:")
-    print ("The following locations are: Sacramento, Elk Grove, Rancho Cordova, Citrus Heights, Natomas, Galt, and East Sacramento.")
+    print ("The following locations are: Elk Grove, Rancho Cordova, Citrus Heights, Natomas, Galt, and East Sacramento.")
     Redirectto = input("\nPlease type a location to go to or type in 'Exit' to exit: ")
